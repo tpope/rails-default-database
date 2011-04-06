851ae9b2a3e437f7b77a853306f6f1f33bee6c00
hoverlover <hoverlover@gmail.com>
2011-04-06 11:28:34 -0500

    Allow override of individual environment sections

--- lib/rails-default-database.rb
+++ lib/rails-default-database.rb
@@ -1,8 +1,18 @@
 Rails::Application::Configuration.class_eval do
 
   def database_configuration_with_default
-    database_configuration_without_default
-  rescue Errno::ENOENT
+    config_file = begin
+      database_configuration_without_default
+    rescue Errno::ENOENT
+      {}
+    end
+
+    default_config.merge(config_file)
+  end
+
+  private
+
+  def default_config
     name = File.basename(root)
     generator = begin
       require 'pg'
